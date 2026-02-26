b06d1db8b72f7cf1148204b1c52fc35d5b591c7d
范仲淹 <237609391+ISFZY@users.noreply.github.com>
2026-02-26 23:16:41 +0800

    Update bootstrap.sh
    Xray-Reality
    
    Signed-off-by: 范仲淹 <237609391+ISFZY@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ==============================================================================
-# Project: https://github.com/ISFZY/Xray-Auto
+# Project: https://github.com/ISFZY/Xray-Reality
 # Author : ISFZY
 # ==============================================================================
 
@@ -13,7 +13,7 @@ PLAIN='\033[0m'
 
 # 定义安装目录
 INSTALL_DIR="xray-install"
-REPO_URL="https://github.com/ISFZY/Xray-Auto.git"
+REPO_URL="https://github.com/ISFZY/Xray-Reality.git"
 
 # 1. 分支选择逻辑 (Branch Selection)
 #    优先读取用户传入的第一个参数 (例如: bash bootstrap.sh dev)
